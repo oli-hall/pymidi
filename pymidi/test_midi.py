92afac36c666b374d978303b00c73225bebcde56
Oli Hall <oli.hall1309@gmail.com>
2019-03-04 17:43:32 +0000

    Rewrite the variable length function
    This didn't deal with any variable length numbers that didn't fit in 2 bytes. In addition, some of the test cases were incorrect. This isn't the most efficient implementation, but should do the trick for now.

--- pymidi/test_midi.py
+++ pymidi/test_midi.py
@@ -7,12 +7,60 @@ from pymidi.utils import variable_length_field
 
 class MidiTest(unittest.TestCase):
 
-    def test_variable_length_decoding(self):
+    def test_variable_length_decoding_of_0_returns_correct_result(self):
+        input = "0x00"
+
+        remainder, extracted = variable_length_field(input)
+
+        self.assertEqual(extracted, 0)
+        self.assertEqual(remainder, BitArray())
+
+    def test_variable_length_decoding_of_127_returns_correct_result(self):
+        input = "0x7F"
+
+        remainder, extracted = variable_length_field(input)
+
+        self.assertEqual(extracted, 127)
+        self.assertEqual(remainder, BitArray())
+
+    def test_variable_length_decoding_of_128_returns_correct_result(self):
+        input = "0x8100"
+
+        remainder, extracted = variable_length_field(input)
+
+        self.assertEqual(extracted, 128)
+        self.assertEqual(remainder, BitArray())
+
+    def test_variable_length_decoding_of_1000_returns_correct_result(self):
+        input = "0x8768"
+
+        remainder, extracted = variable_length_field(input)
+
+        self.assertEqual(extracted, 1000)
+        self.assertEqual(remainder, BitArray())
+
+    def test_variable_length_decoding_of_16383_returns_correct_result(self):
         input = "0xFF7F"
 
         remainder, extracted = variable_length_field(input)
 
         self.assertEqual(extracted, 16383)
+        self.assertEqual(remainder, BitArray())
+
+    def test_variable_length_decoding_of_1000000_returns_correct_result(self):
+        input = "0xBD8440"
+
+        remainder, extracted = variable_length_field(input)
+
+        self.assertEqual(extracted, 1000000)
+        self.assertEqual(remainder, BitArray())
+
+    def test_variable_length_decoding_of_268435455_returns_correct_result(self):
+        input = "0xFFFFFFFF"
+
+        remainder, extracted = variable_length_field(input)
+
+        self.assertEqual(extracted, 268435455)
         self.assertEqual(remainder, BitArray())
 
 
--- pymidi/utils.py
+++ pymidi/utils.py
@@ -1,19 +1,27 @@
 from bitstring import BitArray
 
 
+# TODO make this take a BitArray
+# TODO flip output order
 def variable_length_field(data):
-    # return any spare data?
     bits = BitArray(data)
-
-    extracted = BitArray(bin="00")
+    value = ""
 
     for i in range(0, len(bits), 8):
         byte = bits[i:i + 8]
-        if byte[0]:
-            extracted.append(byte[1:])
-        else:
-            extracted.append(byte[1:])
-            return bits[i + 8:], extracted.int
+        value += byte[1:].bin
+        if not byte[0]:
+            break
 
-    # TODO improve edge-case handling
-    raise Exception("Not a variable length field")
+    padding = "0"*_padding(len(value))
+    value = padding + value
+
+    return bits[i + 8:], BitArray(bin=value).int
+
+
+# TODO there must be a better way to do this
+def _padding(length):
+    if length % 8 != 0:
+        floor = length // 8
+        return ((floor + 1) * 8) - length
+    return 0
