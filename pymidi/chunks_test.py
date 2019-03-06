1095fc6c604d96f07795438d82a5516f1ae72608
Oli Hall <oli.hall1309@gmail.com>
2019-03-06 15:28:52 +0000

    Add tests for header parsing

--- pymidi/chunks_test.py
+++ pymidi/chunks_test.py
@@ -23,3 +23,37 @@ class ChunksTest(unittest.TestCase):
         self.assertEqual(chunks[0]["type"], "header")
         for chk in chunks[1:]:
             self.assertEqual(chk["type"], "track")
+
+    def test_parsing_format_0_file_parses_header_correctly(self):
+        with open(FORMAT_0_EXAMPLE, "rb") as f:
+            chunks = parse_chunks(f)
+
+        self.assertEqual(len(chunks), 2)
+
+        header = chunks[0]
+
+        self.assertEqual(header["type"], "header")
+        self.assertEqual(header["format"], 0)
+        self.assertEqual(header["track_count"], 1)
+
+        division = header["division"]
+
+        self.assertEqual(division["format"], "time units per quarter note")
+        self.assertEqual(division["time_units"], 96)
+
+    def test_parsing_format_1_file_parses_header_correctly(self):
+        with open(FORMAT_1_EXAMPLE, "rb") as f:
+            chunks = parse_chunks(f)
+
+        self.assertEqual(len(chunks), 5)
+
+        header = chunks[0]
+
+        self.assertEqual(header["type"], "header")
+        self.assertEqual(header["format"], 1)
+        self.assertEqual(header["track_count"], 4)
+
+        division = header["division"]
+
+        self.assertEqual(division["format"], "time units per quarter note")
+        self.assertEqual(division["time_units"], 96)
